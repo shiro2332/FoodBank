8a0a4cb422309e644de96f9dd3569d68f1822e3c
Shiro <68656146+shiro2332@users.noreply.github.com>
2021-09-05 01:32:51 +0800

    Update firestore.js

--- public/script/firestore.js
+++ public/script/firestore.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB6k1Q91ZUeRsKneY7El6adwQ1CONAt650",
+    authDomain: "freefoodforyou-6dc88.firebaseapp.com",
+    projectId: "freefoodforyou-6dc88",
+    storageBucket: "freefoodforyou-6dc88.appspot.com",
+    messagingSenderId: "1097439533486",
+    appId: "1:1097439533486:web:5dd7c3ce83cc6cf2ff503f",
+    measurementId: "G-K2439YYX8Q"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 function addMessage() {
     db.collection("ContactMessage").add({
         name: document.getElementById("messageName").value,
