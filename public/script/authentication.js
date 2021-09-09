6446773fd498a7914fb3ec18c5300192a76991dc
Shiro <68656146+shiro2332@users.noreply.github.com>
2021-09-09 17:11:07 +0800

    1

--- public/script/authentication.js
+++ public/script/authentication.js
@@ -1,17 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyB6k1Q91ZUeRsKneY7El6adwQ1CONAt650",
-  authDomain: "freefoodforyou-6dc88.firebaseapp.com",
-  projectId: "freefoodforyou-6dc88",
-  storageBucket: "freefoodforyou-6dc88.appspot.com",
-  messagingSenderId: "1097439533486",
-  appId: "1:1097439533486:web:5dd7c3ce83cc6cf2ff503f",
-  measurementId: "G-K2439YYX8Q"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 const auth = firebase.auth();
 
 //signup function
