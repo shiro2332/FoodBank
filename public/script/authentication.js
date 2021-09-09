3c9b637eae9c1a09f53073a2cdb1f397bcd97bda
Shiro <68656146+shiro2332@users.noreply.github.com>
2021-09-10 02:53:07 +0800

    Update authentication.js

--- public/script/authentication.js
+++ public/script/authentication.js
@@ -2,7 +2,32 @@ const auth = firebase.auth();
 
 function loginWithGoogle() {
   var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
+  auth.signInWithRedirect(provider);
+  auth.getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+    alert("Logged in");
+    window.location.href = "userMenu.html";
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
 }
 
 //signup function
@@ -25,7 +50,7 @@ function signIn() {
       // Signed in
       var user = userCredential.user;
       alert("Logged in");
-      window.location.href = "searchBank.html";
+      window.location.href = "userMenu.html";
       // ...
     })
     .catch((error) => {
