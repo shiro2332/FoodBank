6c187c3ccaa3a304bcb3808c07aa55a7c6a75b48
Shiro <68656146+shiro2332@users.noreply.github.com>
2021-11-18 18:57:25 +0800

    fixed notification

--- public/script/authentication.js
+++ public/script/authentication.js
@@ -62,6 +62,7 @@ function signIn() {
         window.location.href = "adminMenu.html";
       } else {
         alert("Logged in");
+        sessionStorage.setItem('firsttimelogin', true);
         window.location.href = "userMenu.html";
       }
       // ...
